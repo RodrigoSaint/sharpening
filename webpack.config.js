cd37840b8dbd486ddc92e53e1d5691ab198eac86
Rodrigo Santos <rodrigo.saint01@live.com>
2021-07-06 12:25:13 +0200

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(process.cwd(), "source/index.tsx"),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    libraryTarget: "umd",
+    globalObject: "this",
+    path: path.resolve(process.cwd(), "build"),
+  },
+  externals: {
+    react: "react",
+    "styled-components": "styled-components",
+  },
+};
